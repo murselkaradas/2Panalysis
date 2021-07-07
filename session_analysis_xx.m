a03dfecf9195db632f4ed031715a1d610cde3bb4
murselkaradas <72932630+murselkaradas@users.noreply.github.com>
2021-07-06 21:37:15 -0400

    Update session_analysis_xx.m

--- session_analysis_xx.m
+++ session_analysis_xx.m
@@ -11,28 +11,29 @@ addpath 'V:\2P_rigs\2ndrig\2P_Analysis\2Panalysis'
 addpath 'V:\2P_rigs\2ndrig\2P_Analysis\2Panalysis\2P_splitSVD'
 addpath 'V:\2P_rigs\2ndrig\2P_Analysis\2Panalysis\findfirst'
 %% 
-sn='session_analysis_210619.m';
+sn='session_analysis_210702.m';
 %{
-MK18881_210624_field1_avg_00001_00001.tif = Ref
-
-Block 1
- Power: None
- Hologram: None
- Odors: [EthylValerate, 2-Heptanone, Cinnamaldehyde, Methylvalerate] + blank
- Conc: [0.1, 0.01]
- Tif: MK18881_210624_field1_odor_00001_00001.tif
- Wavesurfer: MK18881_210624_field1_odor_001_0001.tif
- Voyeur: MK18881_1_01_D2021_6_24T14_33_8_odor.h5
- Result:
- Notes: 10 trials per odor per conc.
+flows = [ # MFCs , Dilutor
+         [[400,100], [1,1]],
+         [[490,10], [1, 1]]
+]
+self.blockrepeats = 15
+self.stimrepeats = 1
+self.iti_bounds = [10000,10001]
+stimuli = []
+self.trial_structure = 'Passive'
+self.trialdur = 2000
+odors = ['Menthone', 'empty']
+concs = [0.05, 0.1]
 %}
+%%
 %%
 %%  FIELD 1
 % Read H5 and roi file
-fieldname = 'MK18881_210624_field1';
-img_format = [512, 512];
-fps = 30;
-OdorDuration = 1;
+fieldname = 'MK18882_210702_rightbulbBENZ';
+img_format = [256, 256];
+fps = 58.18;
+OdorDuration = 2;
 
 H5Name = dir([strcat(fieldname,'*.h5')]);
 path_h5=H5Name.folder;
@@ -69,6 +70,8 @@ opt = 1;
 img(:,:,2) = img(:,:,2)+double(logical(cellMask1))*0.1;
 figure(22);imagesc(img);CenterFromRoiMasks(cellMask1,1:num_cell,opt);axis square
 savefig(strcat(fieldname, 'ROI', '.fig'))
+%%
+%%
 %% Find Included Trials
 Nframe = size(frametrigger,1);
 pre_inh=floor(2*fps);
@@ -76,6 +79,9 @@ post_inh=floor(4*fps);
 inh_onset = Data.inh_onset;
 up_sampling_fac=(1000/fps);
 ind = 1;
+F = [];
+dff = [];
+meanAll = [];
 for tr=1:length(inh_onset)
     inh=inh_onset(tr);
     inh_frame=find(frametrigger<inh, 1, 'last' );%frame in which inh_onset is included
@@ -104,13 +110,13 @@ end
 %%
 OdorInfo = HDF5_getOdors(path_h5,h5_name,trials_read);
 Sniff_trial = sniff(trials_read,:)./peak2peak(sniff(trials_read,:),2);
-dfflim = [-1 1];
+dfflim = [-2 3];
 framelim = -pre_inh:post_inh-1;
 cellid = 1:num_cell;
-sniff_ds = downsample(Sniff_trial', fps);
+sniff_ds = downsample(Sniff_trial', round(1000/fps));
 for i = 1: size(OdorInfo.odors,1)
     figure(90)
-    subplot(3,3,i)
+    subplot(2,2,i)
     dff1 = mean(dff(:,:,OdorInfo.odorTrials{i}),3);
     imagesc(framelim, cellid, dff1)
     title(OdorInfo.odors{i})
@@ -125,13 +131,13 @@ for i = 1: size(OdorInfo.odors,1)
     hold off
     
     figure(91)
-    subplot(3,3,i)
+    subplot(2,2,i)
     plot(framelim, dff1', 'LineWidth',1.5)
     hold on
     ylim(dfflim)
     plot([1 1]*0, ylim, '--b', 'LineWidth',1)
     plot([1 1]*OdorDuration*fps, ylim, '--b','LineWidth',1)
-    plot(downsample((-pre:post-1),fps)./fps, mean(sniff_ds(:,OdorInfo.odorTrials{i}),2)-0.75, 'k', 'LineWidth',1)
+    plot(downsample((-pre:post-1),round(1000/fps))./round(1000/fps), mean(sniff_ds(:,OdorInfo.odorTrials{i}),2)-1, 'k', 'LineWidth',1)
     hold off
     title(OdorInfo.odors{i})
     xlabel('Frame number')
@@ -146,15 +152,13 @@ savefig(strcat(fieldname, '_DFFTrace', '.fig'))
 
 
 
+%%  LEFT SIDE
+fieldname = 'MK18882_210702_leftbulbBENZ';
+img_format = [256, 256];
+fps = 58.18;
+OdorDuration = 2;
 
-%% ODOR2
-% Read H5 and roi file
-fieldname = 'MK18881_210624_field1';
-img_format = [512, 512];
-fps = 30;
-OdorDuration = 1;
-
-H5Name = dir([strcat(fieldname,'_1_02_','*.h5')]);
+H5Name = dir([strcat(fieldname,'*.h5')]);
 path_h5=H5Name.folder;
 h5_name= H5Name.name;
 layer='MT';
@@ -172,7 +176,7 @@ post = 4000;
 
 %% Read TIFF files and generate fluorescence signal per ROI
 import ScanImageTiffReader.ScanImageTiffReader;
-Names = dir([strcat(fieldname,'_odor_00002_','*.tif')]);
+Names = dir([strcat(fieldname,'*.tif')]);
 filenames = {Names.name};
 foldername = {Names.folder};
 filenum = size(Names,1);
@@ -189,6 +193,7 @@ opt = 1;
 img(:,:,2) = img(:,:,2)+double(logical(cellMask1))*0.1;
 figure(22);imagesc(img);CenterFromRoiMasks(cellMask1,1:num_cell,opt);axis square
 savefig(strcat(fieldname, 'ROI', '.fig'))
+%%
 %% Find Included Trials
 Nframe = size(frametrigger,1);
 pre_inh=floor(2*fps);
@@ -196,6 +201,9 @@ post_inh=floor(4*fps);
 inh_onset = Data.inh_onset;
 up_sampling_fac=(1000/fps);
 ind = 1;
+F = [];
+dff = [];
+meanAll = [];
 for tr=1:length(inh_onset)
     inh=inh_onset(tr);
     inh_frame=find(frametrigger<inh, 1, 'last' );%frame in which inh_onset is included
@@ -222,16 +230,15 @@ for tr=1:length(inh_onset)
 end
 
 %%
-fieldname = 'MK18881_210624_field1_2';
 OdorInfo = HDF5_getOdors(path_h5,h5_name,trials_read);
 Sniff_trial = sniff(trials_read,:)./peak2peak(sniff(trials_read,:),2);
-dfflim = [-1 1];
+dfflim = [-2 3];
 framelim = -pre_inh:post_inh-1;
 cellid = 1:num_cell;
-sniff_ds = downsample(Sniff_trial', fps);
+sniff_ds = downsample(Sniff_trial', round(1000/fps));
 for i = 1: size(OdorInfo.odors,1)
     figure(90)
-    subplot(3,3,i)
+    subplot(2,2,i)
     dff1 = mean(dff(:,:,OdorInfo.odorTrials{i}),3);
     imagesc(framelim, cellid, dff1)
     title(OdorInfo.odors{i})
@@ -246,13 +253,13 @@ for i = 1: size(OdorInfo.odors,1)
     hold off
     
     figure(91)
-    subplot(3,3,i)
+    subplot(2,2,i)
     plot(framelim, dff1', 'LineWidth',1.5)
     hold on
     ylim(dfflim)
     plot([1 1]*0, ylim, '--b', 'LineWidth',1)
     plot([1 1]*OdorDuration*fps, ylim, '--b','LineWidth',1)
-    plot(downsample((-pre:post-1),fps)./fps, mean(sniff_ds(:,OdorInfo.odorTrials{i}),2)-0.75, 'k', 'LineWidth',1)
+    plot(downsample((-pre:post-1),round(1000/fps))./round(1000/fps), mean(sniff_ds(:,OdorInfo.odorTrials{i}),2)-1, 'k', 'LineWidth',1)
     hold off
     title(OdorInfo.odors{i})
     xlabel('Frame number')
@@ -264,4 +271,3 @@ figure(90)
 savefig(strcat(fieldname, '_DFFMAP', '.fig'))
 figure(91)
 savefig(strcat(fieldname, '_DFFTrace', '.fig'))
-
